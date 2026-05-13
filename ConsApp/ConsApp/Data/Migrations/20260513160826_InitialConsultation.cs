f8f0431e2cdd11d23d17b1aa91e4db0d81d37896
Erik Mägi <erimag@taltech.ee>
2026-05-13 19:08:56 +0300

    migrations

---
+++ ConsApp/ConsApp/Data/Migrations/20260513160826_InitialConsultation.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialConsultation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "ConsultationRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Details",
+                table: "ConsultationRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Timestamp",
+                table: "ConsultationRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ValidFrom",
+                table: "ConsultationRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ValidTo",
+                table: "ConsultationRoles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ConsultationUsers",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RoleId",
+                table: "ConsultationUserRoles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ConsultationUserRoles",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ConsultationRoles",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "ConsultationUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleId",
+                table: "ConsultationUserRoles",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "ConsultationUserRoles",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "ConsultationRoles",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "ConsultationRoles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Details",
+                table: "ConsultationRoles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Timestamp",
+                table: "ConsultationRoles",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidFrom",
+                table: "ConsultationRoles",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ValidTo",
+                table: "ConsultationRoles",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
